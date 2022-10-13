c52c13370bf7c07760ff982c89b021b4522efc27
Ikelamps <ikelamps@gmail.com>
2022-10-13 14:34:37 -0700

    C Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print  them all
+ *
+ * Return: Always 0 (All Correct)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0)
+}
