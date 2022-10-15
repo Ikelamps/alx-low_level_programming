967c66d649b65de9e2bea6ab7fe3faa32835ed67
Ikelamps <ikelamps@gmail.com>
2022-10-15 15:56:25 -0700

    C add to variable

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - assign a random number to variable v each time it is run
+ * print out last digit of stored number in v
+ * Return: Always 0 (Brav0)
+ */
+int main(void)
+{
+	int v;
+
+	for (v = 0; v < 10; v++)
+	{
+		printf("%d", v);
+	}
+	printf('\n');
+	return (0);
+}
