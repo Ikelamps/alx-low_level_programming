1e08bde83a80a648a3c5507a5f3a4643a497a936
Ikelamps <ikelamps@gmail.com>
2022-10-15 14:41:06 -0700

    C Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints out lower case alphabets
+ *
+ * Return: Always 0 (Bravo)
+ */
+int main(void)
+{
+	char letters;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
