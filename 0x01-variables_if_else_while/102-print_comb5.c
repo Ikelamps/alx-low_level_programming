bce2663fe2c814ae52db3df29b07a8b80c05369b
Ikelamps <ikelamps@gmail.com>
2022-10-16 06:29:05 -0700

    C PrintComb% Putchar

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of two digit numbers
+ * ranging froo 0 to 99 separated by comma followed by space
+ *
+ * Return: Always 0 (Bravo)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; b < 100; a++)
+	{
+	for (b = 0; b < 100; b++)
+	{
+		if (a < b)
+		{
+		putchar((a / 10) + 48);
+		putchar((a % 10) + 48);
+		putchar(' ');
+		putchar((b / 10) + 48);
+		putchar((b % 10) + 48);
+		if (a != 98 || b != 99)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		}
+		}
+		}
+		putchar('\n');
+		return (0);
+}
