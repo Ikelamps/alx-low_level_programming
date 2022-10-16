1a9945d74849040438ed95fe7b238e4eee328d72
Ikelamps <ikelamps@gmail.com>
2022-10-16 00:34:32 -0700

    C Print Numbz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit of base 10 from 0 using only
+ * putchar without char variable
+ *
+ * Return: Always 0 (Bravo)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num % 10 + '0');
+	putchar('\n');
+	return (0);
