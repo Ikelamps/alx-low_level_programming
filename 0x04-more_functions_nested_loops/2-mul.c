6d684cd4649fdfa9cb1c7ea2b279bc95e6700dbc
Ikelamps <ikelamps@gmail.com>
2022-10-20 13:41:19 -0700

    C 2 Mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * mul - multitude of integers
  * @a: first integer
  * @b: second integer
  * Return: addition of aand b
  */
-int mut(int a, int b)
+int mul(int a, int b)
 {
 	return (a * b);
 }
