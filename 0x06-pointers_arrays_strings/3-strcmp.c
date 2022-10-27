8205cc2113206aa0675f271501579ccb1c7fb1fe
Ikelamps <ikelamps@gmail.com>
2022-10-27 12:33:47 -0700

    C PAS2 Str Cmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,23 @@
+#include "main.h"
 
+/**
+ * _strcmp - compares two string value
+ * @s1: string 1
+ * @s2: string 2
+ * Return: differnce btwns1 and s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int b;
+
+	b = 0;
+	while (s1[b] != '\0' && s2[b] != '\0')
+	{
+		if (s1[b] != s2[b])
+		{
+			return (s1[b] - s2[b]);
+		}
+		b++;
+	}
+	return (0);
+}
