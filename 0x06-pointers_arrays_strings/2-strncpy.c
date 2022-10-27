17a56813f1fbe75e6a3900d234e9fbf40d2b7490
Ikelamps <ikelamps@gmail.com>
2022-10-27 12:18:53 -0700

    C PAS2 Str nCopy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * _strncpy - copy a string with n numbers
+ * _strncpy - copies string with n numbers
  * @dest: copy to
  * @src: copy from
  * @n: number of char to be copied
  * Return: destination
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n);
 {
 	int q;
 
 	q = 0;
-	while; (q < n && src[q] != '\0')
+	while (q < n && src[q] != '\0')
 	{
-		dest[j] = src[q];
+		dest[q] = src[q];
 		q++;
 	}
 	while (q < n)
