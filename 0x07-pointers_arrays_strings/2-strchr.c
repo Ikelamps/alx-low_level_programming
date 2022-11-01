a02374050c2dd2885d13013cfbfb473fb46ad5ca
Ikelamps <ikelamps@gmail.com>
2022-11-01 15:23:02 -0700

    C PAS Str Chr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,16 +10,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s)
+	int i = 0;
 
+	for (; s[i] >= '\0'; i++)
 	{
-		if (*s != c)
-			s++;
-		else
-			return (s);
+		if (s[i] == c)
+			return (&s[i]);
 	}
-	if (c == '\0')
-		return (s);
-
-	return (NULL);
+	return (0);
 }
