a8d41a931e78551ef532d13a018cddfeb908f267
Ikelamps <ikelamps@gmail.com>
2022-11-01 16:32:17 -0700

    Crack Me Plus

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * set_strin - sets the value of apointer to a char
+ * set_string - sets the value of apointer to a char
  * @s: pointer to change
  * @to: string to change pointer to
  *
