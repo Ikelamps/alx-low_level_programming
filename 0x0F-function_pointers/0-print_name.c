6cd05687a85f8dfb8dd11f973b61cd0562e46503
Ikelamps <ikelamps@gmail.com>
2022-11-17 10:41:11 -0800

    C Fun Pon

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,2 @@
-#include "function_pointers.h"
-
-/**
- * print_name - a function that prints a name
- * @name: the name
- * @f: function of name
- * Return: nothing
- */
-void print_name(char *name, void (*f)(char *))
-{
-	if (name && f)
-
-		f(name);
-}
+#include 
+	
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,17 +1,8 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - function given as parameter on each element of an array
- *  @array: the array to execute function on
- *  @size: size of the array
- *  @action: a pointer to the function you need to use
- *  Return: void
- */
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-	unsigned int i;
-
-	if (array && action)
-		for (i = 0; i < size; i++)
-			action(array[i]);
-}
+ * array_iterator - executes a function given as a
+ * parameter on each element of an array
+ * @array: input integer array
+ * @size: size of the array
+ * @action: pointe
