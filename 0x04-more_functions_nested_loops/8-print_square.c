c821b41096a8c4ced50efbadf56751ae732b16b8
Ikelamps <ikelamps@gmail.com>
2022-11-20 08:33:13 +0000

    MFNL fix

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * print_square - Prints a square
+ * print_square - prints a square of size size
  * @size: size of the square
+ * Return: void
  */
+
+
 void print_square(int size)
 {
+	int x, y;
+
 	if (size <= 0)
+		_putchar('\n');
+
+	for (x = 0; x < size; x++)
 	{
+		for (y = 0; y < (size); y++)
+		{
+			_putchar('#');
+		}
 		_putchar('\n');
 	}
-	else
-	{
-		int i, j;
 
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < size; j++)
-			{
-				_puchar('#');
-			}
-			_putchar('\n');
-		}
-	}
 }
